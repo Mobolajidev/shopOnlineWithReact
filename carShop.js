38225e2692de3b2400dd7bd8ea68e9cc53239c04
David Akoki <mobolajidev@gmail.com>
2020-11-16 22:01:30 +0100

    initial commit

---
+++ carShop.js
@@ -0,0 +1,50 @@
+const CARS = [
+    
+    {
+        id: 0,
+        name: 'Voiture',
+        image: 'images/car1.jpg',
+        label: 'USED',
+        price: '$1000'
+    },
+    
+    {
+        id: 1,
+        name: 'Rockie',
+        image: 'images/car2.jpg',
+        label: 'NEW',
+        price: '$3500'
+    },
+    
+    {
+        id: 2,
+        name: 'Smooth',
+        image: 'images/car3.jpg',
+        label: 'NEW',
+        price: '$5000'
+    },
+    
+    {
+        id: 4,
+        name: 'Sleek',
+        image: 'images/car4.jpg',
+        label: 'NEW',
+        price: '$6700'
+    },
+    
+    {
+        id: 5,
+        name: 'Beetle',
+        image: 'images/car7.jpg',
+        label: 'USED',
+        price: '$9800'
+    },
+    
+    {
+        id: 6,
+        name: 'Goofie',
+        image: 'images/car9.jpg',
+        label: 'NEW',
+        price: '$300'
+    },
+]
